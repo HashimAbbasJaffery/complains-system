2abd65c025c1872bfdf9ec02106eca68ae4e64af
hashimAbbasJaffery <habbas2121@outlook.com>
2025-01-28 18:57:50 +0500

    Misc: Dynamic static path

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,17 +9,17 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/black_logo.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/assets/images/black_logo.png') }}">
     <title>
         {{ config('app.name', 'Laravel') }}
     </title>
     <!-- Custom CSS -->
-    <link href="/dist/css/style.min.css" rel="stylesheet">
+    <link href="{{ asset('/dist/css/style.min.css') }}" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js' }}"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js' }}"></script>
 <![endif]-->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://kit.fontawesome.com/3a7e8b6e65.js" crossorigin="anonymous"></script>
@@ -37,18 +37,18 @@
     <!-- ============================================================== -->
     <!-- All Jquery -->
     <!-- ============================================================== -->
-    <script src="/assets/libs/jquery/dist/jquery.min.js"></script>
+    <script src="{{ asset('/assets/libs/jquery/dist/jquery.min.js') }}"></script>
     <!-- Bootstrap tether Core JavaScript -->
-    <script src="/assets/libs/popper.js/dist/umd/popper.min.js"></script>
-    <script src="/assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="{{ asset('/assets/libs/popper.js/dist/umd/popper.min.js') }}"></script>
+    <script src="{{ asset('/assets/libs/bootstrap/dist/js/bootstrap.min.js') }}"></script>
     <!-- slimscrollbar scrollbar JavaScript -->
-    <script src="/assets/extra-libs/sparkline/sparkline.js"></script>
+    <script src="{{ asset('/assets/extra-libs/sparkline/sparkline.js') }}"></script>
     <!--Wave Effects -->
-    <script src="/dist/js/waves.js"></script>
+    <script src="{{ asset('/dist/js/waves.js') }}"></script>
     <!--Menu sidebar -->
-    <script src="/dist/js/sidebarmenu.js"></script>
+    <script src="{{ asset('/dist/js/sidebarmenu.js') }}"></script>
     <!--Custom JavaScript -->
-    <script src="/dist/js/custom.min.js"></script>
+    <script src="{{ asset('/dist/js/custom.min.js') }}"></script>
 </body>
 
 </html>
